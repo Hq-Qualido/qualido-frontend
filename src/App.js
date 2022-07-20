872de83573ec3e739834923f5fa569b3cda4044d
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2022-07-20 20:35:39 +0530

    updated import

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Products from "./components/products/Products";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductId from "./components/products/ProductId";
 import Error from "./components/error/Error";
-import Login from "./components/login/Login";
-import Register from "./components/login/Register";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import Cart from "./components/cart/Cart";
 
 function App() {
