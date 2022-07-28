15c8b62cc71c55044608a1cd805eb7bbf54b61d9
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2022-07-28 19:46:55 +0530

    Update Categories.js

--- src/components/homepage/Categories.js
+++ src/components/homepage/Categories.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Categories(props) {
-  console.log(props.name,"props");
-
   return (
     <>
       <div className="category-item">
