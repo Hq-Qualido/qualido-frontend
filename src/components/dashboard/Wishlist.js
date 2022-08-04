679469d3a56d2106af5d869f228bb0b535e9f7cb
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2022-08-04 19:26:28 +0530

    empty wishlist

--- src/components/dashboard/Wishlist.js
+++ src/components/dashboard/Wishlist.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Wishlist() {
   return (
-    <div>Wishlist</div>
+    <>
+    <div className="container wishlist-page">
+        <lottie-player
+        src="https://assets4.lottiefiles.com/private_files/lf30_e3pteeho.json"
+        background="transparent"
+        speed="1"
+        style={{width: "280px", height: "280px"}}
+        loop
+        autoplay
+      ></lottie-player>
+      <p className='wishlist-para'>Your wishlist is empty!! <br /> Browse our products and add to your collections.</p>
+    <Link to="/products" className="info-btn">Add   Items</Link>
+    </div>
+    </>
   )
 }
