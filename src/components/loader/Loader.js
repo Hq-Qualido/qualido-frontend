e5abed7d006f734a7552d575dcdbfc6fdcf07401
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2022-08-19 19:39:28 +0530

    css loader

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Loader.css'
+
+export default function Loader() {
+  return (
+    <div>
+    <div className="spinner">
+        <div className="dot">
+
+        </div>
+    </div>
+    </div>
+  )
+}
