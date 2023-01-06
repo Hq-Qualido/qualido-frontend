532df5c01d647db6a023d747fbd2c5b05718e3ad
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2023-01-06 20:16:50 +0530

    Auth error checking

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,121 +1,140 @@
-import React from 'react'
-import './Login.css'
+import React from "react";
+import "./Login.css";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { baseUrl } from "../../BaseUrl";
 
-import LoginGirl from '../../assets/loginGirl.png'
-import google from '../../assets/google.png'
-import { useState } from 'react';
-import Loader from '../loader/Loader';
+import LoginGirl from "../../assets/loginGirl.png";
+import google from "../../assets/google.png";
+import { useState } from "react";
 
 export default function Login() {
   const [loading, setLoading] = useState(false);
 
-    const [values,setValues] = useState({
-        email:"",
-        password:"",
-    })
-    const navigate = useNavigate();
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState({
+    emailError: "",
+    passwordError: "",
+  });
 
-    const handleChange =(e)=>{
-        const {name,value} = e.target;
-        setValues({
-            ...values,
-            [name]:value,
-        })
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+  async function handleSubmit() {
+    if (values.email.length < 3) {
+      setError({ ...error, emailError: "Please Enter Valid Email!" });
+    } else setError({ ...error, emailError: "" });
+
+    if (error.emailError.length > 0 ) {
+    //   console.log(values);
+      setLoading(true);
+      const response = await fetch(`${baseUrl}/auth/login`, {
+        method: "POST",
+        body: JSON.stringify({
+          email: values.email,
+          password: values.password,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      setLoading(false);
+    //   console.log(data);
+      if (data.token) {
+        localStorage.setItem("Name", data.user.fullname);
+        // console.log(localStorage);
+        navigate("/dashboard");
+        window.location.reload();
+      } else {
+        navigate("/signup");
+      }
     }
-    async function handleSubmit(){
-        console.log(values)
-        setLoading(true);
-              const response = await fetch(
-           `${baseUrl}/auth/login`,
-           {
-             method: "POST",
-             body: JSON.stringify({
-             email:values.email,
-             password:values.password,
-             }),
-             headers: {
-               'Content-Type': 'application/json',
-             }
-           })
-           const data =await response.json();
-        setLoading(false);
-           console.log(data)
-           if(data.token){
-            localStorage.setItem('Name',data.user.fullname)
-            console.log(localStorage);
-            navigate("/dashboard");
-            window.location.reload();
-           }
-           else{
-            navigate("/signup");
-           }
-    }
+  }
 
   return (
     <>
-    <div className="container-fluid loginPage">
+      <div className="container-fluid loginPage">
         <div className="row">
-            <div className="loginPage-left col-lg-6 col-sm-6">
-            <div className="register-here mb-2">New User? 
-            <Link to="/signup"> Register Here</Link>
-             </div>
-            <p>Join us and stay tuned to all <br />  the updates and notifications!</p>
-                <img src={LoginGirl} alt="LoginGirl" />
+          <div className="loginPage-left col-lg-6 col-sm-6">
+            <div className="register-here mb-2">
+              New User?
+              <Link to="/signup"> Register Here</Link>
             </div>
-            <div className="loginPage-right col-lg-6 col-sm-6">
-                <div className="form">
-                <div className='form-heading my-2'>
-                <h2>Login</h2> 
+            <p>
+              Join us and stay tuned to all <br /> the updates and
+              notifications!
+            </p>
+            <img src={LoginGirl} alt="LoginGirl" />
+          </div>
+          <div className="loginPage-right col-lg-6 col-sm-6">
+            <div className="form">
+              <div className="form-heading my-2">
+                <h2>Login</h2>
                 <p>Welcome onboard with us!</p>
+              </div>
+
+              <form>
+                <label htmlFor="">
+                  E-mail
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Enter your e-mail"
+                    value={values.email}
+                    onChange={handleChange}
+                    autoComplete="off"
+                    required
+                  />
+                  {error.emailError.length > 0 ? (
+                    <div className="error_message">{error.emailError}</div>
+                  ) : (
+                    ""
+                  )}
+                </label>
+                <label htmlFor="">
+                  Password
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Enter your password"
+                    value={values.password}
+                    onChange={handleChange}
+                    autoComplete="off"
+                    required
+                  />
+                  {error.passwordError.length > 0 ? (
+                    <div className="error_message">{error.passwordError}</div>
+                  ) : (
+                    ""
+                  )}
+                </label>
+                <br />
+                <div onClick={handleSubmit} className="dislodged-border">
+                  {!loading ? "Login" : "Wait..."}
                 </div>
 
-                    <form>
-                    <label htmlFor="">E-mail
-                    <input 
-                        type="email" 
-                        name="email"
-                        id="email"
-                        placeholder='Enter your e-mail'
-                        value={values.email}
-                        onChange={handleChange}
-                        autoComplete='off'
-                        required
-                        /> 
-                        </label>
-                    <label htmlFor="">Password
-                    <input 
-                        type="password" 
-                        name="password"
-                        id="password"
-                        placeholder='Enter your password'
-                        value={values.password}
-                        onChange={handleChange}
-                        autoComplete='off'
-                        required
-                        />
-                    </label>
-                     
-                            <br />
-                        <div onClick={handleSubmit} className="dislodged-border">
-                       {!loading ? "Login":
-                        ( <Loader type="text" text="Wait..." /> )
-                        }
-                        </div>
-
-
-                        <div className="with-google my-4">
-                        <Link to="/" style={{textDecoration:"none"}}>
-                            Continue with <img src={google} className="mx-1" alt="" />
-                       </Link>
-                        </div>
-                    </form>
+                <div className="with-google my-4">
+                  <Link to="/" style={{ textDecoration: "none" }}>
+                    Continue with <img src={google} className="mx-1" alt="" />
+                  </Link>
                 </div>
+              </form>
             </div>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,167 +1,199 @@
-import React from 'react'
-import './Login.css'
+import React from "react";
+import "./Login.css";
 import { Link } from "react-router-dom";
-import { useState } from 'react';
-import {FaRegFrown} from 'react-icons/fa'
+import { useState } from "react";
 import { baseUrl } from "../../BaseUrl";
 
-import LoginGirl from '../../assets/loginGirl.png'
-import google from '../../assets/google.png'
-import OtpPopup from './OtpPopup';
+import LoginGirl from "../../assets/loginGirl.png";
+import google from "../../assets/google.png";
+import OtpPopup from "./OtpPopup";
 
 export default function Register() {
-    const [popup,setPopup] = useState(false);
-    const [errors,setErrors] = useState('');
-    const [userData,setUserData] = useState({
-        email:"",
-        fullname:"",
-        createPass:"",
-        confirmPass:"",
-    })
+  const [popup, setPopup] = useState(false);
 
-    const handleChange =(e)=>{
-        const {name,value} = e.target;
-        setUserData({
-            ...userData,
-            [name]:value,
-        })
+  const [fullnameError, setFullnameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [createPassError, setCreatePassError] = useState("");
+  const [confirmPassError, setConfirmPassError] = useState("");
+
+  const [userData, setUserData] = useState({
+    email: "",
+    fullname: "",
+    createPass: "",
+    confirmPass: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
+  async function handleSubmit() {
+
+    if (!userData.email) setEmailError("Email required!");
+    else if (!/\S+@\S+\.\S+/.test(userData.email))
+      setEmailError("Email address is invalid!");
+    else setEmailError("");
+
+    if (userData.fullname.length < 3) setFullnameError("Enter your full name!");
+    else setFullnameError("");
+
+    if (userData.createPass.length <= 6) setCreatePassError("Weak password!");
+    else setCreatePassError("");
+
+    if (userData.confirmPass.length <= 6)
+      setConfirmPassError("Re-enter your password!");
+    else if (userData.createPass !== userData.confirmPass)
+      setConfirmPassError("Passwords did not match!");
+    else setConfirmPassError("");
+
+    if (
+      !emailError &&
+      !fullnameError &&
+      !createPassError &&
+      !confirmPassError
+    ) {
+      const response = await fetch(`${baseUrl}/auth/register`, {
+        method: "POST",
+        body: JSON.stringify({
+          email: userData.email,
+          fullname: userData.fullname,
+          password: userData.confirmPass,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      if(data.token) setPopup(true)
+    //console.log(data,"data")
     }
-    async function handleSubmit(){
-        // console.log(userData);
-        if (!userData.email) {
-            setErrors('Email required!');
-          } else if (!/\S+@\S+\.\S+/.test(userData.email)) {
-            setErrors('Email address is invalid!');
-          }else if (userData.fullname.length < 3) {
-            setErrors('Enter your full name!');
-          }else if( userData.createPass.length<=6){
-            setErrors('Weak password!');
-          }
-        else if (userData.createPass !== userData.confirmPass){
-            setErrors('Passwords did not match!');
-        }
-        else{
-            setErrors('')
-            // console.log(userData.confirmPass)
-        const response = await fetch(`${baseUrl}/auth/register`,
-            {
-                method: "POST",
-                body: JSON.stringify({
-                    email:userData.email ,
-                    fullname:userData.fullname ,
-                    password:userData.confirmPass ,
-                }),
-                headers: {
-                            'Content-Type': 'application/json',
-                           }
-            })
-            const data =await response.json();
-                 console.log(data,"data")       
-        }
-    }
+  }
 
   return (
     <>
-    <div className="container-fluid loginPage">
+      <div className="container-fluid loginPage">
         <div className="row">
-            <div className="loginPage-left col-lg-6 col-sm-6">
-            <div className="register-here mb-2">Already an User? 
-            <Link to="/login"> Login Now</Link>
-             </div>
-            <p>Join us and stay tuned to all <br />  the updates and notifications!</p>
-                <img src={LoginGirl} alt="LoginGirl" />
+          <div className="loginPage-left col-lg-6 col-sm-6">
+            <div className="register-here mb-2">
+              Already an User?
+              <Link to="/login"> Login Now</Link>
             </div>
-            <div className="loginPage-right col-lg-6 col-sm-6">
-                <div className="form">
-                <div className='form-heading my-2'>
-                <h2>Register Now</h2> 
+            <p>
+              Join us and stay tuned to all <br /> the updates and
+              notifications!
+            </p>
+            <img src={LoginGirl} alt="LoginGirl" />
+          </div>
+          <div className="loginPage-right col-lg-6 col-sm-6">
+            <div className="form">
+              <div className="form-heading my-2">
+                <h2>Register Now</h2>
                 <p>Welcome onboard with us!</p>
+              </div>
+              <form>
+                <label htmlFor="email">
+                  E-mail
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={userData.email}
+                    onChange={handleChange}
+                    required
+                    placeholder="Enter your e-mail"
+                    autoComplete="off"
+                  />
+                  {emailError.length > 0 ? (
+                    <div className="error_message">{emailError}</div>
+                  ) : (
+                    ""
+                  )}
+                </label>
+
+                <label htmlFor="fullname">
+                  Your Name
+                  <input
+                    type="text"
+                    name="fullname"
+                    id="fullname"
+                    value={userData.fullname}
+                    onChange={handleChange}
+                    required
+                    placeholder="Enter your full name"
+                    autoComplete="off"
+                  />
+                  {fullnameError.length > 0 ? (
+                    <div className="error_message">{fullnameError}</div>
+                  ) : (
+                    ""
+                  )}
+                </label>
+
+                <label htmlFor="createPass">
+                  Create Password
+                  <input
+                    type="password"
+                    name="createPass"
+                    id="createPass"
+                    value={userData.createPass}
+                    onChange={handleChange}
+                    required
+                    autoComplete="off"
+                    placeholder="Create your password"
+                  />
+                  {createPassError.length > 0 ? (
+                    <div className="error_message">{createPassError}</div>
+                  ) : (
+                    ""
+                  )}
+                </label>
+
+                <label htmlFor="confirmPass">
+                  Confirm Password
+                  <input
+                    type="password"
+                    name="confirmPass"
+                    id="confirmPass"
+                    value={userData.confirmPass}
+                    onChange={handleChange}
+                    required
+                    autoComplete="off"
+                    placeholder="Confirm your password"
+                  />
+                  {confirmPassError.length > 0 ? (
+                    <div className="error_message">{confirmPassError}</div>
+                  ) : (
+                    ""
+                  )}
+                </label>
+
+                <br />
+
+                <div
+                  className="dislodged-border"
+                  onClick={(e) => {
+                    handleSubmit(e);
+                  }}
+                >
+                  Signup
                 </div>
-                    <form >
-                    
-                    <label htmlFor="email">E-mail
-                    <input 
-                        type="email" 
-                        name="email"
-                        id="email"
-                        value={userData.email}
-                        onChange={handleChange}
-                        required
-                        placeholder='Enter your e-mail'
-                        autoComplete='off'
-                        className={errors.length>0? "error":""}
-                        /> 
-                        </label>
 
-
-                    <label htmlFor="fullname">Your Name
-                    <input 
-                        type="text" 
-                        name="fullname"
-                        id="fullname"
-                        value={userData.fullname}
-                        onChange={handleChange}
-                        required
-                        placeholder='Enter your full name'
-                        autoComplete='off'
-                        /> 
-                        </label>
-
-
-                    <label htmlFor="createPass">Create Password
-                    <input 
-                        type="password" 
-                        name="createPass"
-                        id="createPass"
-                        value={userData.createPass}
-                        onChange={handleChange}
-                        required
-                        autoComplete='off'
-                        placeholder='Create your password'
-                        />
-                    </label>
-                     
-                    <label htmlFor="confirmPass">Confirm Password
-                    <input 
-                        type="password" 
-                        name="confirmPass"
-                        id="confirmPass"
-                        value={userData.confirmPass}
-                        onChange={handleChange}
-                        required
-                        autoComplete='off'
-                        placeholder='Confirm your password'
-                        />
-                    </label>
-                     
-                            <br />
-                          { errors.length>0 &&  <div className="error-box "> <span className= 'mx-1'> <FaRegFrown /> 
-                          </span>
-                            { errors }
-                          </div>
-                           }
-
-
-                                <div className="dislodged-border" 
-                        onClick={(e)=>{
-                            setPopup(true);
-                            handleSubmit(e);
-                            }}>
-                            Signup
-                            </div>
-
-                        <div className="with-google my-4">
-                       <Link to="/" style={{textDecoration:"none"}}>
-                            Continue with <img src={google} className="mx-1" alt="" />
-                       </Link>
-                        </div>
-                    </form>
+                <div className="with-google my-4">
+                  <Link to="/" style={{ textDecoration: "none" }}>
+                    Continue with <img src={google} className="mx-1" alt="" />
+                  </Link>
                 </div>
+              </form>
             </div>
+          </div>
         </div>
-    </div>
-{ errors.length<=0 &&
-    <OtpPopup trigger={popup} userData={userData} setTrigger={setPopup}/>}
+      </div>
+      {popup && (
+        <OtpPopup trigger={popup} userData={userData} setTrigger={setPopup} />
+      )}
     </>
-  )
+  );
 }
