b1bc1b858adc74c3ee4eb0fb7f0afbe57f6375fd
prateek-srivastav <prateek.srvastav@gmail.com>
2023-01-09 23:15:08 +0530

    fixed

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -72,138 +72,139 @@ export default function Register() {
       //console.log(data,"data")
     }
   }
-}
 
-const handleGoogleLogin = async () => {
-  const response = await fetch(`${baseUrl}/auth/google/url`);
+  const handleGoogleLogin = async () => {
+    const response = await fetch(`${baseUrl}/auth/google/url`);
 
-  const data = await response.json();
+    const data = await response.json();
 
-  console.log(data);
-  window.location.replace(data.url);
-};
+    console.log(data);
+    window.location.replace(data.url);
+  };
 
-return (
-  <>
-    <div className="container-fluid loginPage">
-      <div className="row">
-        <div className="loginPage-left col-lg-6 col-sm-6">
-          <div className="register-here mb-2">
-            Already an User?
-            <Link to="/login"> Login Now</Link>
-          </div>
-          <p>
-            Join us and stay tuned to all <br /> the updates and notifications!
-          </p>
-          <img src={LoginGirl} alt="LoginGirl" />
-        </div>
-        <div className="loginPage-right col-lg-6 col-sm-6">
-          <div className="form">
-            <div className="form-heading my-2">
-              <h2>Register Now</h2>
-              <p>Welcome onboard with us!</p>
+  return (
+    <>
+      <div className="container-fluid loginPage">
+        <div className="row">
+          <div className="loginPage-left col-lg-6 col-sm-6">
+            <div className="register-here mb-2">
+              Already an User?
+              <Link to="/login"> Login Now</Link>
             </div>
-            <form>
-              <label htmlFor="email">
-                E-mail
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  value={userData.email}
-                  onChange={handleChange}
-                  required
-                  placeholder="Enter your e-mail"
-                  autoComplete="off"
-                />
-                {emailError.length > 0 ? (
-                  <div className="error_message">{emailError}</div>
-                ) : (
-                  ""
-                )}
-              </label>
-
-              <label htmlFor="fullname">
-                Your Name
-                <input
-                  type="text"
-                  name="fullname"
-                  id="fullname"
-                  value={userData.fullname}
-                  onChange={handleChange}
-                  required
-                  placeholder="Enter your full name"
-                  autoComplete="off"
-                />
-                {fullnameError.length > 0 ? (
-                  <div className="error_message">{fullnameError}</div>
-                ) : (
-                  ""
-                )}
-              </label>
-
-              <label htmlFor="createPass">
-                Create Password
-                <input
-                  type="password"
-                  name="createPass"
-                  id="createPass"
-                  value={userData.createPass}
-                  onChange={handleChange}
-                  required
-                  autoComplete="off"
-                  placeholder="Create your password"
-                />
-                {createPassError.length > 0 ? (
-                  <div className="error_message">{createPassError}</div>
-                ) : (
-                  ""
-                )}
-              </label>
-
-              <label htmlFor="confirmPass">
-                Confirm Password
-                <input
-                  type="password"
-                  name="confirmPass"
-                  id="confirmPass"
-                  value={userData.confirmPass}
-                  onChange={handleChange}
-                  required
-                  autoComplete="off"
-                  placeholder="Confirm your password"
-                />
-                {confirmPassError.length > 0 ? (
-                  <div className="error_message">{confirmPassError}</div>
-                ) : (
-                  ""
-                )}
-              </label>
-
-              <br />
-
-              <div
-                className="dislodged-border"
-                onClick={(e) => {
-                  setPopup(true);
-                  handleSubmit(e);
-                }}
-              >
-                Signup
+            <p>
+              Join us and stay tuned to all <br /> the updates and
+              notifications!
+            </p>
+            <img src={LoginGirl} alt="LoginGirl" />
+          </div>
+          <div className="loginPage-right col-lg-6 col-sm-6">
+            <div className="form">
+              <div className="form-heading my-2">
+                <h2>Register Now</h2>
+                <p>Welcome onboard with us!</p>
               </div>
+              <form>
+                <label htmlFor="email">
+                  E-mail
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={userData.email}
+                    onChange={handleChange}
+                    required
+                    placeholder="Enter your e-mail"
+                    autoComplete="off"
+                  />
+                  {emailError.length > 0 ? (
+                    <div className="error_message">{emailError}</div>
+                  ) : (
+                    ""
+                  )}
+                </label>
 
-              <div className="with-google my-4">
-                <Link to="/" style={{ textDecoration: "none" }}>
-                  Continue with <img src={google} className="mx-1" alt="" />
-                </Link>
-              </div>
-            </form>
+                <label htmlFor="fullname">
+                  Your Name
+                  <input
+                    type="text"
+                    name="fullname"
+                    id="fullname"
+                    value={userData.fullname}
+                    onChange={handleChange}
+                    required
+                    placeholder="Enter your full name"
+                    autoComplete="off"
+                  />
+                  {fullnameError.length > 0 ? (
+                    <div className="error_message">{fullnameError}</div>
+                  ) : (
+                    ""
+                  )}
+                </label>
+
+                <label htmlFor="createPass">
+                  Create Password
+                  <input
+                    type="password"
+                    name="createPass"
+                    id="createPass"
+                    value={userData.createPass}
+                    onChange={handleChange}
+                    required
+                    autoComplete="off"
+                    placeholder="Create your password"
+                  />
+                  {createPassError.length > 0 ? (
+                    <div className="error_message">{createPassError}</div>
+                  ) : (
+                    ""
+                  )}
+                </label>
+
+                <label htmlFor="confirmPass">
+                  Confirm Password
+                  <input
+                    type="password"
+                    name="confirmPass"
+                    id="confirmPass"
+                    value={userData.confirmPass}
+                    onChange={handleChange}
+                    required
+                    autoComplete="off"
+                    placeholder="Confirm your password"
+                  />
+                  {confirmPassError.length > 0 ? (
+                    <div className="error_message">{confirmPassError}</div>
+                  ) : (
+                    ""
+                  )}
+                </label>
+
+                <br />
+
+                <div
+                  className="dislodged-border"
+                  onClick={(e) => {
+                    setPopup(true);
+                    handleSubmit(e);
+                  }}
+                >
+                  Signup
+                </div>
+
+                <div className="with-google my-4">
+                  <Link to="/" style={{ textDecoration: "none" }}>
+                    Continue with <img src={google} className="mx-1" alt="" />
+                  </Link>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    {popup && (
-      <OtpPopup trigger={popup} userData={userData} setTrigger={setPopup} />
-    )}
-  </>
-);
+      {popup && (
+        <OtpPopup trigger={popup} userData={userData} setTrigger={setPopup} />
+      )}
+    </>
+  );
+}
