9fe51c016cffb4c716074062c44bf4cf6aac63cd
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2023-02-17 18:18:03 +0530

    Prod images

--- src/components/products/ProductId.js
+++ src/components/products/ProductId.js
@@ -9,8 +9,8 @@ import Footer from "../footer/Footer";
 import { Link, useParams } from "react-router-dom";
 import { useLocation } from "react-router";
 import { useCart } from "react-use-cart";
-import Loader from "../loader/Loader";
 import { baseUrl } from "../../BaseUrl";
+import ProductImages from "./ProductImages";
 
 export default function ProductId() {
   let { productId } = useParams();
@@ -60,54 +60,11 @@ export default function ProductId() {
       <div className="container my-5">
         <div className="row">
           <div className="col-lg-4 col-sm-6 productID-left">
-            {indivProd.urls ? (
-              <div
-                className="col-md-auto d-flex align-items-center justify-content-center rounded rounded-lg"
-                style={{ height: "50vh", width: "100%",overflow:"auto" }}
-              >
-                <img
-                  style={{ height: "45vh", width: "auto" }}
-                  src={prodImage}
-                  alt="BookImage"
-                />
-              </div>
-            ) : (
-              <Loader type="dots" />
-            )}
-
-            <div
-              className="d-flex flex-row p-1 d-flex align-items-center justify-content-center"
-              style={{ width: "100%", overflowX: "auto" }}
-            >
-              {indivProd.urls &&
-                indivProd.urls.map((item, index) => {
-                  return (
-                    <div
-                      key={index}
-                      onClick={() => {
-                        setProdImage(item);
-                      }}
-                      style={{
-                        width: "50px",
-                        height: "55px",
-                        backgroundColor: "rgb(250, 250, 250)",
-                        overflow: "hidden",
-                      }}
-                      className={`${
-                        item === prodImage
-                          ? " shadow-sm border border-2 border-warning rounded mx-1 d-flex align-items-center justify-content-center p-1"
-                          : "rounded mx-1 d-flex align-items-center justify-content-center p-1"
-                      }`}
-                    >
-                      <img
-                        src={item}
-                        alt="img"
-                        style={{ height: "45px", width: "auto" }}
-                      />
-                    </div>
-                  );
-                })}
-            </div>
+            <ProductImages
+              indivProd={indivProd}
+              prodImage={prodImage}
+              setProdImage={setProdImage}
+            />
 
             <div className="product-buttons my-4">
               {checkItemInCart ? (
