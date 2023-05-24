0b101d2bde7b1d342f4685581fb56b43800795ac
prateek-srivastav <prateek.srvastav@gmail.com>
2023-05-25 00:14:19 +0530

    gauth fix2

--- src/components/utils/ProtectedRoutes.jsx
+++ src/components/utils/ProtectedRoutes.jsx
@@ -4,9 +4,10 @@ import useToken from "../../hooks/useToken";
 import { useCart } from "react-use-cart";
 import { useEffect } from "react";
 import cartApi from "../../api/cart";
+import authApi from "../../api/auth";
 
 export function ProtectedRoutes() {
-  const { token } = useToken();
+  const { token, setName, setToken } = useToken();
   const location = useLocation();
 
   const { setItems, items } = useCart();
@@ -17,7 +18,23 @@ export function ProtectedRoutes() {
     setItems(res.data[0].items);
   };
 
+  const cookieValue = document.cookie
+    .split("; ")
+    .find((cookie) => cookie.startsWith("auth_token"))
+    ?.split("=")[1];
+
+  const fetchData = async () => {
+    const res = await authApi.getUser(cookieValue);
+    setName(res.data.user.fullname);
+    setToken(res.data.token);
+    return window.location.replace("/dashboard");
+  };
+
+  // useEffect(() => {
+  // }, []);
+
   useEffect(() => {
+    if (cookieValue && !token) fetchData();
     getCart();
   }, []);
 
