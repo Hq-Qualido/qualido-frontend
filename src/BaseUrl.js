1269e3c0a81b9dba6fc0f20fd597b89d091f6791
prateek-srivastav <prateek.srvastav@gmail.com>
2023-05-25 12:50:37 +0530

    changed base url

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -1,5 +1,5 @@
 // const baseUrl = "https://api.qualido.in/api"
-const baseUrl = "https://qualido-server-16td.onrender.com/api";
+const baseUrl = "https://api.qualido.in/api";
 // const baseUrl = "http://localhost:8080/api";
 
 export { baseUrl };
